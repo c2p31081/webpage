0f87f16f8b8af162a27d3f876fac13a71c6575aa
C2P31501 <114720678+alexwongxxx@users.noreply.github.com>
2023-12-06 15:49:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 from flask import Flask, render_template
-import requests
+import requests 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # 地震情報のAPIエンドポイント
+    # リアルタイム地震データの取得
     p2pquake_url = 'https://api.p2pquake.net/v2/history?codes=551&limit=1'
-    
-    # APIから地震情報を取得
-    try:
-        response = requests.get(p2pquake_url)
-        response.raise_for_status()  # ステータスコードが200以外の場合はエラーを発生させる
-        earthquake_info = response.json()[0]  # 最新の地震情報を取得
-    except requests.RequestException as e:
-        earthquake_info = None
-        print(f"Error fetching earthquake data: {e}")
+    response = requests.get(p2pquake_url)
+    real_time_data = response.json()[0] if response.status_code == 200 else None
 
-    # 地震情報が取得できたかチェック
-    if earthquake_info:
-        # 震央地名
-        eq_name = earthquake_info["earthquake"]["hypocenter"]["name"]
-        # 震度
-        max_intensity = earthquake_info["earthquake"]["maxScale"]
-        # 日時
-        eq_datetime = earthquake_info["earthquake"]["time"]
-    else:
-        eq_name = max_intensity = eq_datetime = "データが取得できませんでした"
+    # テストデータの定義
+    test_data = [
+      {
+        "earthquake": {
+          "time": "2023-12/06-12:00:00",
+          "hypocenter": {
+            "name": "神奈川県茅ヶ崎市",
+            "magnitude": 3.4,
+            "maxScale": 3,
+            "depth": 10,
+            "latitude": 35.3302,
+            "longitude": 139.4053
+          }
+        }
+      }
+    ]
 
-    # 地震情報をHTMLテンプレートに渡してレンダリング
-    return render_template('index.html', eq_name=eq_name, max_intensity=max_intensity, eq_datetime=eq_datetime)
+    # テンプレートにリアルタイムデータとテストデータを渡す
+    return render_template('info.html', real_time_data=real_time_data, test_data=test_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
